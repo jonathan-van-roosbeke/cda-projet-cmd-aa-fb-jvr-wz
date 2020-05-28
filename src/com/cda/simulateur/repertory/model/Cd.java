5a2354e7373f982dd29a9b8b7d66fbfab2e70180
Van Roosbeke <jonathan.van.roosbeke@gmail.com>
2020-05-28 20:06:41 +0200

    update commande cd et pwd

--- src/com/cda/simulateur/repertory/model/Cd.java
+++ src/com/cda/simulateur/repertory/model/Cd.java
@@ -1,7 +1,22 @@
 package com.cda.simulateur.repertory.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.cda.simulateur.menu.action.Command;
 
-public class Cd extends Command {
+public final class Cd extends Command {
 
+	public static void changeDirectory(String pCommande) {
+		if(pCommande.equals("..") || pCommande.equalsIgnoreCase("cd..") && Pwd.vCheminArray.size() > 1) {
+			Pwd.vCheminArray.remove(Pwd.vCheminArray.size() - 1);
+		} else if(pCommande.equals(".")) {
+			
+		} else {
+			List<String> temp = Arrays.asList(pCommande.split("\\\\"));
+			for(String s : temp) {
+				Pwd.vCheminArray.add(s);
+			}
+		}
+	}
 }
--- src/com/cda/simulateur/repertory/model/Pwd.java
+++ src/com/cda/simulateur/repertory/model/Pwd.java
@@ -1,7 +1,22 @@
 package com.cda.simulateur.repertory.model;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import com.cda.simulateur.menu.action.Command;
 
-public class Pwd extends Command {
+public final class Pwd extends Command {
 
+	private static final String vCheminDeDebut = System.getProperty("user.dir");
+	protected static List<String> vCheminArray = new LinkedList<>(Arrays.asList(vCheminDeDebut.split("\\\\")));
+	
+	public static String returnChemin() {
+		StringBuilder sb = new StringBuilder();
+		for(String s : vCheminArray) {
+			sb.append(s);
+			sb.append("\\");
+		}
+		return sb.toString().substring(0, sb.length()-1);
+	}
 }
