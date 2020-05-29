2c2c01d09b143456d22cb6191384263d6bd24ec0
Van Roosbeke <jonathan.van.roosbeke@gmail.com>
2020-05-29 10:21:58 +0200

    update commande cd pwd et ls

--- src/com/cda/simulateur/repertory/model/Cd.java
+++ src/com/cda/simulateur/repertory/model/Cd.java
@@ -1,5 +1,6 @@
 package com.cda.simulateur.repertory.model;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,16 +8,47 @@ import com.cda.simulateur.menu.action.Command;
 
 public final class Cd extends Command {
 
-	public static void changeDirectory(String pCommande) {
-		if(pCommande.equals("..") || pCommande.equalsIgnoreCase("cd..") && Pwd.vCheminArray.size() > 1) {
+	public static void executer(String pSaisie) {
+		
+		// va au parent si la commande == ".." || "cd.."
+		if (pSaisie.equals("..") || pSaisie.equalsIgnoreCase("cd..") && Pwd.vCheminArray.size() > 1) {
 			Pwd.vCheminArray.remove(Pwd.vCheminArray.size() - 1);
-		} else if(pCommande.equals(".")) {
 			
-		} else {
-			List<String> temp = Arrays.asList(pCommande.split("\\\\"));
-			for(String s : temp) {
+		} else if (pSaisie.equals(".")) {
+			//on ne fait rien, on reste dans le répertoire actuel
+			
+		// si le repertoire absolue existe on efface le Pwd et on met le nouveau chemin	
+		} else if (verifieSiCheminAbsolueExiste(pSaisie)) {
+			Pwd.vCheminArray.clear();
+			List<String> temp = Arrays.asList(pSaisie.split("\\\\"));
+			for (String s : temp) {
 				Pwd.vCheminArray.add(s);
 			}
+		
+		// on verfie si le repertoire demande existe dans le repertoire actuel
+		} else if (verifieSiLeDocumentExisteDansRepertoireCourant(pSaisie)) {
+			Pwd.vCheminArray.add(pSaisie);
 		}
+		
+		else {
+			System.out.println("le repertoire que vous demandez est introuvable \n");
+		}
+	}
+
+	private static boolean verifieSiCheminAbsolueExiste(String pSaisie) {
+		File file = new File(pSaisie);
+		if (file.isDirectory()) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean verifieSiLeDocumentExisteDansRepertoireCourant(String pSaisie) {
+		String tempChemin = Pwd.executer() + "\\" + pSaisie;
+		File file = new File(tempChemin);
+		if (file.isDirectory()) {
+			return true;
+		}
+		return false;
 	}
 }
--- src/com/cda/simulateur/repertory/model/Pwd.java
+++ src/com/cda/simulateur/repertory/model/Pwd.java
@@ -1,7 +1,7 @@
 package com.cda.simulateur.repertory.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 import com.cda.simulateur.menu.action.Command;
@@ -9,9 +9,10 @@ import com.cda.simulateur.menu.action.Command;
 public final class Pwd extends Command {
 
 	private static final String vCheminDeDebut = System.getProperty("user.dir");
-	protected static List<String> vCheminArray = new LinkedList<>(Arrays.asList(vCheminDeDebut.split("\\\\")));
+	protected static List<String> vCheminArray = new ArrayList<>(Arrays.asList(vCheminDeDebut.split("\\\\")));
 	
-	public static String returnChemin() {
+	// retourne le chemin du repertoire courant
+	public static String executer() {
 		StringBuilder sb = new StringBuilder();
 		for(String s : vCheminArray) {
 			sb.append(s);
