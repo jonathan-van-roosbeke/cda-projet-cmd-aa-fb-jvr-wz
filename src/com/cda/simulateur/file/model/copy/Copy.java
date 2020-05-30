92996826dcad43e755c910dddb9b2a3783d27116
Wei <zhuw17@hotmail.com>
2020-05-30 14:12:28 +0200

    commit update

--- src/com/cda/simulateur/file/model/copy/Copy.java
+++ src/com/cda/simulateur/file/model/copy/Copy.java
@@ -6,10 +6,6 @@ public class Copy extends Command {
 	private static final String ID = "copy";
 	private static final String DESC = "Copie un fichier";
 
-//	public boolean executer() {
-//		return true;
-//	}
-
 	Copy() {
 		super(ID, DESC);
 	}
