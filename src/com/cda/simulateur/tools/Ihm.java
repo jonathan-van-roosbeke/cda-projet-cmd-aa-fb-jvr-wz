eb5bbc55e606f1599e5aeab73395ac9f3a17d328
Wei <zhuw17@hotmail.com>
2020-05-31 17:50:48 +0200

    find ok

--- src/com/cda/simulateur/tools/Ihm.java
+++ src/com/cda/simulateur/tools/Ihm.java
@@ -16,6 +16,7 @@ import com.cda.simulateur.minijeux.River;
 import com.cda.simulateur.repertory.model.Cd;
 import com.cda.simulateur.repertory.model.Dir;
 import com.cda.simulateur.repertory.model.Dirng;
+import com.cda.simulateur.repertory.model.Find;
 import com.cda.simulateur.repertory.model.Ls;
 import com.cda.simulateur.repertory.model.Pwd;
 
@@ -39,6 +40,8 @@ public class Ihm {
 		listCmd.put("dir", Dir.dirInstance);
 		listCmd.put("dirng", Dirng.dirngInstance);
 		listCmd.put("copy", Copy.copyInstance);
+		listCmd.put("find", Find.findInstance);
+
 	}
 
 	public static HashMap<String, Command> getListCmd() {
