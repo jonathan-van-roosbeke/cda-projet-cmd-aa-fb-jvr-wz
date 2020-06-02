dfa8268cf96bf62e7b0db9b9bb38b8edcfc6de12
Antoine Auzoux <antoine.auzoux@gmail.com>
2020-06-02 10:11:13 +0200

    modif sur main pour lancement jar avec paramètre

--- src/com/cda/simulateur/prog/ProgrammeSimulateurConsole.java
+++ src/com/cda/simulateur/prog/ProgrammeSimulateurConsole.java
@@ -1,16 +1,20 @@
 package com.cda.simulateur.prog;
 
+import java.io.File;
+
 import com.cda.simulateur.tools.Ihm;
 
 public class ProgrammeSimulateurConsole {
 
 	public static void main(String[] args) {
 		String proprieteLancement = System.getProperty("cdi.default.folder");
+		File testFichier = new File(proprieteLancement);
 
-		if (proprieteLancement != null) {
-			System.setProperty("user.dir", proprieteLancement);
+		if (proprieteLancement != null && testFichier.exists()) {
+			if (!(testFichier.isFile())) {
+				System.setProperty("user.dir", proprieteLancement);
+			}
 		}
-
 		Ihm.lancerMenu();
 	}
 }
